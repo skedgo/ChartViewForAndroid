05bafc142b46b55018ab48968756c2d9592bc34c
Trung <congtrungvnit@gmail.com>
2015-01-21 12:07:10 +0700

    Move sources to new module

--- app/src/main/java/com/skedgo/pricingchart/MainActivity.java
+++ app/src/main/java/com/skedgo/pricingchart/MainActivity.java
@@ -2,15 +2,10 @@ package com.skedgo.pricingchart;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 
 public class MainActivity extends ActionBarActivity {
 

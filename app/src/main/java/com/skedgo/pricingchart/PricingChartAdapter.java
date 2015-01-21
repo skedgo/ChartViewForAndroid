3d110fc40573b99d786afd362a8cce7f687ef3c4
Trung <congtrungvnit@gmail.com>
2015-01-21 11:47:06 +0700

    Create chart view module

--- app/src/main/java/com/skedgo/pricingchart/PricingChartAdapter.java
+++ app/src/main/java/com/skedgo/pricingchart/PricingChartAdapter.java
@@ -2,7 +2,6 @@ package com.skedgo.pricingchart;
 
 import android.os.Build;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
